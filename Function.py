6345348a3e14736dfadf50264fb1c74e5fe3e076
Natasm <31316300+Natasm@users.noreply.github.com>
2019-11-13 12:17:26 -0300

    Add methods computational mathematics

---
+++ Function.py
@@ -0,0 +1,8 @@
+import math
+
+#Taylor -> x + 1
+#Posicao falsa -> x^3 - 9x + 3
+#Newton Method -> x^2 - 7
+
+def function_value(x):
+    return math.sin(x) - 1
---
+++ main.py
@@ -0,0 +1,16 @@
+import Lagrange_Polynomial, Numerical_Differentiation,\
+       False_Position, Newton_Method, Trapezoid_Method, Function, \
+       Simpson_Method, Taylor_Polynomial, Gauss_Legendre
+
+#print(Lagrange_Polynomial.lagrange_([2, 2.5, 4], 3))
+#print(Numerical_Differentiation.derivative_first_h2(3, 0.00000001))
+
+#print(False_Position.false_position(0, 1, 0.001, 0.001))
+#print(Newton_Method.newton_method(3, 0.0000001))
+
+#print(Trapezoid_Method.trapezoid_method_compost(0, 2, 100))
+#print(Simpson_Method.simpson_method_compost(0,1, 6))
+
+#print(Taylor_Polynomial.taylor_polynomial(0, 1, 2, 0.001))
+
+#print(Gauss_Legendre.gauss_legendre_order_2(2,6))
